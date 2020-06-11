07fcc19f9e29c9637b67065cffa4e2ec579cd556
Kiefer Ares <kiefer_aresii@dlsu.edu.ph>
2020-06-11 05:36:59 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board.any?{ |f| f.nil?} || board.any?{|e| e.eq? ""} || board[index] == " "
+  if board.any?{ |f| f.nil?} || board.any?{|e| e.eql? ""} || board[index] == " "
     false
   else
     true
